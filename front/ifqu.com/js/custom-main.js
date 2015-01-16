4ff19466e7e7dbbdd0b76e616b5747de41038dc3
bao.wang <bao.wang@qunar.com>
2015-01-16 16:01:42 +0800

    new

---
+++ front/ifqu.com/js/custom-main.js
@@ -0,0 +1,88 @@
+﻿/***************************************************
+			CUSTOM SCRIPTS FOR MHOME PAGE
+***************************************************/
+
+
+
+
+/***************************************************
+			CarouFredSel
+***************************************************/
+jQuery.noConflict()(function($){
+$("#cl-carousel").carouFredSel({
+    auto        : false,
+    width		: 480,
+    height		: 106,
+    scroll		: {
+    	items		: 2,
+    	duration    : 500
+/*    	fx			: "fade"*/
+    },
+	prev    : {
+        button  : "#cl-carousel_prev",
+        key     : "left"
+    },
+    next    : {
+        button  : "#cl-carousel_next",
+        key     : "right"
+    }
+
+	});
+	
+$("#rp-carousel").carouFredSel({
+	circular: false,
+    infinite: false,
+    auto    : false,
+/*    width		: 720,*/
+    width		: 480,
+    height		: 213,
+    scroll		: {
+    	items		: 2,
+    	duration    : 500
+    },
+	prev    : {
+        button  : "#rp-carousel_prev",
+        key     : "left"
+    },
+    next    : {
+        button  : "#rp-carousel_next",
+        key     : "right"
+    }
+	});
+
+$("#ra-carousel").carouFredSel({
+	circular: false,
+    infinite: false,
+    auto    : false,
+    width		: 480,
+    height		: 213,
+    scroll		: {
+    	items		: 2,
+    	duration    : 500
+    },
+	prev    : {
+        button  : "#ra-carousel_prev",
+        key     : "left"
+    },
+    next    : {
+        button  : "#ra-carousel_next",
+        key     : "right"
+    }
+	});
+
+$("#testmain_carousel").carouFredSel({
+    auto        : false,
+    width		: 440,
+    height		: "auto",
+    items		: 1,
+    scroll		: {
+    	items		: 1,
+    	fx			: "fade"
+    },
+    auto		: {
+    	delay		: 3000,
+    	pauseDuration : 5000
+    }
+	});
+
+});
---
+++ front/ifqu.com/js/custom.js
@@ -0,0 +1,98 @@
+﻿/***************************************************
+			SUPER FISH MENU
+***************************************************/
+jQuery.noConflict()(function($){
+$(document).ready(function() {
+   $("ul.sf-menu").superfish({ 
+            pathClass:  'current',
+			autoArrows	: false,
+			delay:300,
+			speed: 'normal',
+			animation:   {opacity:'show'}
+
+        }); 
+
+
+    });
+
+/***************************************************
+					Flickr
+***************************************************/
+	
+	$('#cbox').jflickrfeed({
+		limit: 9,
+		qstrings: {
+			id: '47257185@N03'
+		},
+		itemTemplate: '<li>'+
+						'<a rel="colorbox" href="{{image_b}}" title="{{title}}">' +
+							'<img src="{{image_s}}" alt="{{title}}" />' +
+						'</a>' +
+					  '</li>'
+	}, function(data) {
+		$('#cbox a').colorbox();
+	});
+	
+/* Sidebar widget */	
+	$('#cbox-sidebar').jflickrfeed({
+		limit: 9,
+		qstrings: {
+			id: '47257185@N03'
+		},
+		itemTemplate: '<li>'+
+						'<a rel="colorbox" href="{{image_b}}" title="{{title}}">' +
+							'<img src="{{image_s}}" alt="{{title}}" />' +
+						'</a>' +
+					  '</li>'
+	}, function(data) {
+		$('#cbox-sidebar a').colorbox();
+	});
+	
+});
+
+/***************************************************
+					Item ColorBox
+***************************************************/
+jQuery.noConflict()(function($){
+$('#slider-item a').colorbox({rel:'project-item'});
+});
+
+/***************************************************
+			Item ColorBox - Recent Projects
+***************************************************/
+jQuery.noConflict()(function($){
+$(document).ready(function() {	
+	$('.mask a[data-rel="zoom-img"]').colorbox();
+	});	
+});
+
+/***************************************************
+					Twitter
+***************************************************/
+jQuery.noConflict()(function($){
+$(document).ready(function() {
+
+	
+	  /*---- Footer Twitter----*/
+	  $("#footer .tweet").tweet({
+        	count: 3,
+        	username: "zesky",
+        	loading_text: "loading twitter..."      
+		});
+
+	  /*---- Sidebar Twitter ----*/
+	  $("#twitter-widget .tweet").tweet({
+        	count: 3,
+        	username: "envato",
+        	loading_text: "loading twitter..."      
+		});
+
+	  /*---- Homepage Twitter ----*/
+	  $("#top-gag .tweet").tweet({
+        	count: 1,
+        	username: "zesky",
+        	loading_text: "loading twitter..."      
+		});
+
+	 });
+});
